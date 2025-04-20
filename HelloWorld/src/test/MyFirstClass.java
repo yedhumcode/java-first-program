c9834c99d8d62201c9489d0d0fa404d11f40b503
Yedhu Mohan <yedhu@bams.local>
2025-04-20 10:37:42 +0400

    comment added

--- HelloWorld/src/test/MyFirstClass.java
+++ HelloWorld/src/test/MyFirstClass.java
@@ -14,6 +14,7 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator newobj = new Calculator();
+		// Method call
 		newobj.calcu(7,4, "add");
 	}
 }
